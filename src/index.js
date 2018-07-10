36848400db53e2f9dc3d5a72a1379597563fbe24
Bhavya Kamboj <bhavyakamboj@gmail.com>
2018-07-10 06:49:28 +0530

    Inital react setup with working youtube API

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = "";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards'},(videos)=> {
+            this.setState({ videos });
+            console.log(videos);
+        })
+    }
+
+    render(){
+    return (
+        <div>
+            <SearchBar />
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />,document.querySelector(".container")); 
