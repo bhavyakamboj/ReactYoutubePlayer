14a0d528bd396943776f71c082abea0410f7dd4d
Bhavya Kamboj <bhavyakamboj@gmail.com>
2018-07-14 15:40:17 +0530

    working video player which plays selected video

--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({ key: API_KEY, term: 'sharks'},(videos) => {
-            this.setState({ videos });
+        YTSearch({ key: API_KEY, term: 'perfect home'},(videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+             });
         });
     }
 
@@ -23,7 +29,10 @@ class App extends Component {
     return (
         <div>
             <SearchBar />
-            <VideoList videos={this.state.videos}/> 
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+             onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+             videos={this.state.videos}/> 
         </div>
         )
     }
