3b18c8d4e5b7e9a2f55647907630c20c3708149f
Bhavya Kamboj <bhavyakamboj@gmail.com>
2018-07-14 15:14:37 +0530

    added list and detail view for videos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import Properties from '../config.js';
  
 const API_KEY = Properties();
@@ -12,16 +14,16 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({ key: API_KEY, term: 'surfboards'},(videos)=> {
+        YTSearch({ key: API_KEY, term: 'sharks'},(videos) => {
             this.setState({ videos });
-            console.log(videos);
-        })
+        });
     }
 
     render(){
     return (
         <div>
             <SearchBar />
+            <VideoList videos={this.state.videos}/> 
         </div>
         )
     }
